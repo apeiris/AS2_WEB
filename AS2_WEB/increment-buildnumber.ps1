ee457def133d9a2e14bd4884010eeb4a276031c1
anthony peiris <mapeiris@hotmail.com>
2023-03-09 06:26:11 -0500

    d28

--- AS2_WEB/increment-buildnumber.ps1
+++ AS2_WEB/increment-buildnumber.ps1
@@ -5,6 +5,10 @@ Write-Output "Latest tag: $latestTag"
 $buildNumber = $latestTag.Split('.')[3]
 Write-Output "Current build number: $buildNumber"
 
+$major = 1
+$minor = 0
+$patch = 0
+
 # Increment the build number and update the Git tag
 $newBuildNumber = [int]$buildNumber + 1
 Write-Output "New build number: $newBuildNumber"
@@ -12,5 +16,5 @@ Write-Output "New build number: $newBuildNumber"
 $newTag = "$major.$minor.$patch.$newBuildNumber"
 Write-Output "New tag: $newTag"
 
-#git tag -f $newTag
-#git push --tags
+git tag -f $newTag
+git push --tags
